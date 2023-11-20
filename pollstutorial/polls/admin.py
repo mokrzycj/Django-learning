622eb7fd51660eead1421bcacf0bfe8bab8547b0
mokrzycj <106016822+mokrzycj@users.noreply.github.com>
2023-11-20 18:17:37 +0100

    All files in polls app from django documentation

--- pollstutorial/polls/admin.py
+++ pollstutorial/polls/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
+from .models import Question, Choice
 
 # Register your models here.
 
-from .models import Question
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    # fieldsets = [
+    #     (None, {"fields": ["question_text"]}),
+    #     ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    # ]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    inlines = [ChoiceInline]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+
+
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
--- pollstutorial/polls/views.py
+++ pollstutorial/polls/views.py
@@ -1,8 +1,99 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.template import loader
+from .models import Question, Choice
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+# def index(request):
+#     '''Question.objects odnosi się do modelu Question, a order_by("-pub_date") 
+#     sortuje wyniki malejąco według daty publikacji. 
+#     [:5] oznacza, że interesują nas tylko pierwsze pięć wyników.'''
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+
+#     '''Zmienna q jest wprowadzana w składni list comprehension, 
+#     a Python sam zrozumie, że odnosi się ona do obiektów z listy latest_question_list.
+#     W tym przypadku, q staje się zmienną tymczasową, 
+#     reprezentującą pojedyncze pytanie w każdej iteracji pętli.'''
+#     # output = ", ".join([q.question_text for q in latest_question_list])
+
+#     '''    template = loader.get_template("polls/index.html")
+#     context = {
+#         "latest_question_list": latest_question_list,
+#     }
+
+#     return HttpResponse(HttpResponse(template.render(context, request)))'''
+
+#     # druga metoda zwracania odpowiedniego widoku, używając skrótu Django
+#     context = {"latest_question_list": latest_question_list}
+#     return render(request, "polls/index.html", context)
+
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, "polls/detail.html", {"question": question})
+
+# another way of writing details method
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/detail.html", {"question": question})
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk = question_id)
+#     return render(request, "polls/results.html", {"question": question})
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte = timezone.now()).order_by("-pub_date")[:5]
+    
+class DetailView(generic.DetailView):
+    model=Question
+    template_name = "polls/detail.html"
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+    
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question":question,
+                "error_message": "You didn't select a choice."
+            },
+        )
+    else:
+        selected_choice.votes +=1 
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+    
